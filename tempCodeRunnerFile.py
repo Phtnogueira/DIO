ed1db8532021a28fbaa8bb7f6a273d3f486b96c9
Paulo Henrique T. Nogueira <phv1x@hotmail.com>
2023-08-30 10:00:20 -0300

    commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+/{modelo}/{ano}/{placa}")
+
---
+++ teste.py
@@ -0,0 +1,4 @@
+def carros(marca, modelo, ano, placa):
+    print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
+
+carros("Fiat", "Palio", 1999, "ABC-1234")
